4b65b54bd22a058be9cbd76d613c6d76ae1ba094
Sean Batson <seanbatson@Seans-Mac-Pro.local>
2015-02-24 19:45:22 -0500

    Support for PHFComposerBar

---
+++ Example/BABFrameObservingInput/BABFrameObservingInput.h
@@ -0,0 +1,21 @@
+//
+//  BABFrameObservingInput.h
+//  BABFrameObservingInput
+//
+//  Created by Sean Batson on 2015-02-23.
+//  Copyright (c) 2015 Philipe Fatio. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import <BABFrameObservingInput/BABFrameObservingInputAccessoryView.h>
+
+//! Project version number for BABFrameObservingInput.
+FOUNDATION_EXPORT double BABFrameObservingInputVersionNumber;
+
+//! Project version string for BABFrameObservingInput.
+FOUNDATION_EXPORT const unsigned char BABFrameObservingInputVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <BABFrameObservingInput/PublicHeader.h>
+
+
---
+++ Example/PHFDelegateChain/PHFDelegateChain.h
@@ -0,0 +1,19 @@
+//
+//  PHFDelegateChain.h
+//  PHFDelegateChain
+//
+//  Created by Sean Batson on 2015-02-23.
+//  Copyright (c) 2015 Philipe Fatio. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for PHFDelegateChain.
+FOUNDATION_EXPORT double PHFDelegateChainVersionNumber;
+
+//! Project version string for PHFDelegateChain.
+FOUNDATION_EXPORT const unsigned char PHFDelegateChainVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PHFDelegateChain/PublicHeader.h>
+
+
